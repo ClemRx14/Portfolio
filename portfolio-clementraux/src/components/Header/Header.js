89d75b24200400dd652b613eba37e4f564ab60e9
Clement Raux <ilesquafr@gmail.com>
2025-01-20 20:06:07 +0100

    début header, animation

--- portfolio-clementraux/src/components/Header/Header.js
+++ portfolio-clementraux/src/components/Header/Header.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import SplitText from 'split-text-js';
+import './Header.css';
 
 const Header = () => {
+  useEffect(() => {
+    const titles = gsap.utils.toArray('p');
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+  
+    titles.forEach((title) => {
+      const splitTitle = new SplitText(title, { type: "chars" });
+  
+      tl.from(splitTitle.chars, {
+        opacity: 0,
+        y: 80,
+        rotateX: -90,
+        stagger: 0.02,
+      }, "<")
+      .to(splitTitle.chars, {
+        opacity: 0,
+        y: -80,
+        rotateX: 90,
+        stagger: 0.02,
+      }, "<1");
+    });
+  }, []);
+
   return (
     <header>
-      <h1>Bienvenue sur mon portfolio</h1>
+      <div className="container">
+        <div className="text-wrapper">
+          <p>Clément Raux</p>
+          <p>Frontend Developer</p>
+          <p>UI Designer</p>
+          <p>Performance Web</p>
+        </div>
+      </div>
     </header>
   );
 };
