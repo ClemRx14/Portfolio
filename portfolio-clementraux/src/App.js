16e44b9a0b2be364e46a3d9d261001ec9665ee73
Clement Raux <ilesquafr@gmail.com>
2025-01-22 18:32:27 +0100

    Amélioration Accessibilité

--- portfolio-clementraux/src/App.js
+++ portfolio-clementraux/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
+import About from './sections/About/About.js';
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <About/>
     </div>
   );
 }
