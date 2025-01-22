3643841f010198f4dc38dcd404346d670c158ec2
Clement Raux <ilesquafr@gmail.com>
2025-01-22 18:06:05 +0100

    Responsive Header + Légères AméliorationsSEO et Rich Snippet,

--- portfolio-clementraux/src/components/Header/Header.js
+++ portfolio-clementraux/src/components/Header/Header.js
@@ -36,8 +36,8 @@ const Header = () => {
         </div>
         <nav className="navbar-header">
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#home">Accueil</a></li>
+            <li><a href="#about">A propos</a></li>
             <li><a href="#mesprojets">Mes Projets</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
