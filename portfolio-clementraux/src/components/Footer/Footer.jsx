cb927622e9b040ac27511d21427e117395c8ddfa
Clement Raux <ilesquafr@gmail.com>
2025-01-26 20:11:54 +0100

    aria label for access

--- portfolio-clementraux/src/components/Footer/Footer.jsx
+++ portfolio-clementraux/src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
             <p>&copy; 2025 Raux Clément. Tous droits réservés.</p>
             </div>
             <div className="footer-links">
-                <a href="https://www.linkedin.com/in/clément-raux-836436348" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a>
-                <a href="https://github.com/ClemRx14" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+                <a href="https://www.linkedin.com/in/clément-raux-836436348" target="_blank" rel="noopener noreferrer" aria-label="Visiter mon profil LinkedIn"><FaLinkedin/></a>
+                <a href="https://github.com/ClemRx14" target="_blank" rel="noopener noreferrer" aria-label="Visiter mon profil GitHub"><FaGithub/></a>
             </div>
         </footer>
     )
